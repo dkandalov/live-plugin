c7fd81d6bd6f13acb8be9b1dcac5b6bd44072081
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2011-10-13 19:36:38 +0100

    prototype for opening putty from popup dialog

--- src/ru/intellijeval/sampleplugin/plugin.groovy
+++ src/ru/intellijeval/sampleplugin/plugin.groovy
@@ -3,13 +3,20 @@ package ru.intellijeval.sampleplugin
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.actionSystem.KeyboardShortcut
 import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.keymap.KeymapManager
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.MessageType
+import com.intellij.openapi.ui.popup.JBPopupFactory
+import com.intellij.openapi.util.IconLoader
 import com.intellij.openapi.wm.ToolWindowId
 import com.intellij.openapi.wm.ToolWindowManager
+import javax.swing.Icon
 import javax.swing.KeyStroke
+import com.intellij.openapi.ui.popup.JBPopupFactory.ActionSelectionAid
+
 //import static ru.beans.Bean.*
 
 //-- classpath: c:/work/zz_misc/groovy-beans/target/gbeans-0.0-SNAPSHOT.jar
@@ -28,16 +35,44 @@ def registerAction() {
     actionManager.unregisterAction(actionId)
   }
 
+  Icon ICON = IconLoader.getIcon("/fileTypes/text.png");
   def action = new AnAction() {
     @Override
     void actionPerformed(AnActionEvent e) {
+
+      DefaultActionGroup actionGroup = new DefaultActionGroup();
+
+      Project project = e.project();
+      3.times {
+        actionGroup.add(new AnAction(it.toString(), "Open " + it, ICON) {
+          @Override
+          public void actionPerformed(AnActionEvent e2) {
+            Runtime.runtime.exec("\"c:/Program Files (x86)/putty/putty.exe\" -load \"lonhngprod04\"")
+            showPopup("myaaaaaaaaaaaaaaaaaaaaaaaaaaaAction")
+          }
+        });
+      }
+      def group2 = new DefaultActionGroup("aa", true);
+      group2.add(new AnAction("!!!!!!") {
+        @Override
+        void actionPerformed(AnActionEvent aae) {
+        }
+      })
+      actionGroup.add(group2)
+
+      JBPopupFactory.instance.createActionGroupPopup("pupop", actionGroup, e.dataContext, ActionSelectionAid.SPEEDSEARCH, true).showCenteredInCurrentWindow(project);
+
 //      showPopup("myAction !!! ${MyUtil.sayHi()} ${bean([a: 123])}")
-      showPopup("myAction !!! ${MyUtil.sayHi()}")
+//      showPopup("myaaaaaaaaaaaaaaaaaaaaaaaaaaaAction !!! ${MyUtil.sayHi()} ${e.project()}")
     }
   }
   KeymapManager.instance.activeKeymap.addShortcut(actionId, new KeyboardShortcut(KeyStroke.getKeyStroke("alt shift W"), null))
   actionManager.registerAction(actionId, action)
 }
+
+AnActionEvent.metaClass.project = { delegate.getData(PlatformDataKeys.PROJECT) }
+AnActionEvent.metaClass.editor = { delegate.getData(PlatformDataKeys.EDITOR) }
+AnActionEvent.metaClass.fileText = { delegate.getData(PlatformDataKeys.FILE_TEXT) }
 
 try {
   registerAction()
