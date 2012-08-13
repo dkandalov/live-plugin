789a646aeb27fb053921e2a1d913c386859393c3
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-08-14 00:25:29 +0100

    rough actions to add/remove file or folder disabled syntax highlighting for opened files

---
+++ src/ru/intellijeval/toolwindow/NewElementPopupAction.java
@@ -0,0 +1,55 @@
+package ru.intellijeval.toolwindow;
+
+import com.intellij.icons.AllIcons;
+import com.intellij.ide.IdeBundle;
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.fileChooser.actions.NewFolderAction;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.openapi.ui.popup.ListPopup;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * User: dima
+ * Date: 13/08/2012
+ */
+class NewElementPopupAction extends AnAction implements DumbAware, PopupAction {
+	public void actionPerformed(final AnActionEvent event) {
+		showPopup(event.getDataContext());
+	}
+
+	protected void showPopup(DataContext context) {
+		createPopup(context).showInBestPositionFor(context);
+	}
+
+	protected ListPopup createPopup(DataContext dataContext) {
+		return JBPopupFactory.getInstance().createActionGroupPopup(
+				IdeBundle.message("title.popup.new.element"),
+				getGroup(),
+				dataContext,
+				false,
+				false,
+				false,
+				null,
+				-1,
+				LangDataKeys.PRESELECT_NEW_ACTION_CONDITION.getData(dataContext)
+		);
+	}
+
+	private ActionGroup getGroup() {
+		return new ActionGroup() {
+			@NotNull @Override
+			public AnAction[] getChildren(AnActionEvent e) {
+				return new AnAction[]{
+						new NewFolderAction("Directory", "Directory", AllIcons.Nodes.Folder),
+						new NewFileAction("File", AllIcons.FileTypes.Text)
+				};
+			}
+		};
+	}
+
+	public void update(AnActionEvent e) {
+		Presentation presentation = e.getPresentation();
+		presentation.setEnabled(true);
+	}
+}
--- src/ru/intellijeval/toolwindow/PluginsToolWindow.java
+++ src/ru/intellijeval/toolwindow/PluginsToolWindow.java
@@ -1,12 +1,19 @@
 package ru.intellijeval.toolwindow;
 
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
+import com.intellij.codeInsight.daemon.impl.analysis.FileHighlighingSetting;
+import com.intellij.codeInsight.daemon.impl.analysis.HighlightLevelUtil;
+import com.intellij.ide.ui.customization.CustomizationUtil;
+import com.intellij.lang.Language;
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.fileChooser.FileChooser;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.fileChooser.FileSystemTree;
+import com.intellij.openapi.fileChooser.ex.FileChooserKeys;
 import com.intellij.openapi.fileChooser.ex.FileNodeDescriptor;
 import com.intellij.openapi.fileChooser.ex.FileSystemTreeImpl;
 import com.intellij.openapi.fileEditor.OpenFileDescriptor;
+import com.intellij.openapi.fileTypes.FileTypes;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.project.ProjectManagerListener;
@@ -17,6 +24,11 @@ import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowAnchor;
 import com.intellij.openapi.wm.ToolWindowManager;
 import com.intellij.pom.Navigatable;
+import com.intellij.psi.FileViewProvider;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.util.PsiUtilBase;
 import com.intellij.ui.ScrollPaneFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
@@ -24,12 +36,14 @@ import com.intellij.ui.treeStructure.Tree;
 import com.intellij.util.EditSourceOnDoubleClickHandler;
 import com.intellij.util.EditSourceOnEnterKeyHandler;
 import com.intellij.util.ui.tree.TreeUtil;
+import org.jetbrains.annotations.NonNls;
 import ru.intellijeval.EvalData;
 import ru.intellijeval.EvaluateAction;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -51,7 +65,6 @@ public class PluginsToolWindow {
 
 			@Override
 			public void projectClosed(Project project) {
-				// unregister window in any case
 				unregisterWindowFrom(project);
 			}
 
@@ -78,12 +91,16 @@ public class PluginsToolWindow {
 	private Content createContent(Project project) {
 		myFsTree = createFsTree(project);
 
+		AnAction action = new NewElementPopupAction();
+		action.registerCustomShortcutSet(
+				new CustomShortcutSet(new KeyboardShortcut(KeyStroke.getKeyStroke("ctrl N"), null)), myFsTree.getTree()); // TODO use generic shortcut?
+		CustomizationUtil.installPopupHandler(myFsTree.getTree(), "InetlliJEval.Popup", ActionPlaces.UNKNOWN);
+
 		JScrollPane scrollPane = ScrollPaneFactory.createScrollPane(myFsTree.getTree());
-		panel = new SimpleToolWindowPanel(true).setProvideQuickActions(false);
+		panel = new MySimpleToolWindowPanel(true, myFsTree).setProvideQuickActions(false);
 		panel.add(scrollPane);
 
 		panel.setToolbar(createToolBar());
-
 		return ContentFactory.SERVICE.getInstance().createContent(panel, "", false);
 	}
 
@@ -96,8 +113,9 @@ public class PluginsToolWindow {
 		descriptor.setRoots(roots);
 
 		MyTree myTree = new MyTree(project);
-		EditSourceOnDoubleClickHandler.install(myTree); // handlers must be installed before adding myTree to FileSystemTreeImpl
-		EditSourceOnEnterKeyHandler.install(myTree);
+		// handlers must be installed before adding myTree to FileSystemTreeImpl
+		EditSourceOnDoubleClickHandler.install(myTree, new DisableHighlightingRunnable(project, myFsTree));
+		EditSourceOnEnterKeyHandler.install(myTree, new DisableHighlightingRunnable(project, myFsTree));
 
 		return new FileSystemTreeImpl(project, descriptor, myTree, null, null, null);
 	}
@@ -135,6 +153,23 @@ public class PluginsToolWindow {
 		ToolWindowManager.getInstance(project).unregisterToolWindow(TOOL_WINDOW_ID);
 	}
 
+	private static class MySimpleToolWindowPanel extends SimpleToolWindowPanel {
+		private final FileSystemTreeImpl fileSystemTree;
+
+		public MySimpleToolWindowPanel(boolean vertical, FileSystemTreeImpl fileSystemTree) {
+			super(vertical);
+			this.fileSystemTree = fileSystemTree;
+		}
+
+		@Override public Object getData(@NonNls String dataId) {
+			if (dataId.equals(FileSystemTree.DATA_KEY.getName())) return fileSystemTree;
+			if (dataId.equals(FileChooserKeys.NEW_FILE_TYPE.getName())) return FileTypes.PLAIN_TEXT;
+			if (dataId.equals(FileChooserKeys.DELETE_ACTION_AVAILABLE.getName())) return true;
+			if (dataId.equals(PlatformDataKeys.VIRTUAL_FILE_ARRAY.getName())) return fileSystemTree.getSelectedFiles();
+			return super.getData(dataId);
+		}
+	}
+
 	private static class MyTree extends Tree implements TypeSafeDataProvider {
 		private final Project project;
 
@@ -143,7 +178,7 @@ public class PluginsToolWindow {
 		}
 
 		@Override public void calcData(DataKey key, DataSink sink) {
-			if (PlatformDataKeys.NAVIGATABLE_ARRAY.equals(key)) {
+			if (PlatformDataKeys.NAVIGATABLE_ARRAY.equals(key)) { // need this to be able to open files in toolwindow on double-click/enter
 				List<FileNodeDescriptor> nodeDescriptors = TreeUtil.collectSelectedObjectsOfType(this, FileNodeDescriptor.class);
 				List<Navigatable> navigatables = new ArrayList<Navigatable>();
 				for (FileNodeDescriptor nodeDescriptor : nodeDescriptors) {
@@ -206,6 +241,39 @@ public class PluginsToolWindow {
 				}
 			}
 			e.getPresentation().setEnabled(anyPluginPathSelected);
+		}
+	}
+
+	private static class DisableHighlightingRunnable implements Runnable {
+		private final Project project;
+		private final FileSystemTreeImpl myFsTree;
+
+		public DisableHighlightingRunnable(Project project, FileSystemTreeImpl myFsTree) {
+			this.project = project;
+			this.myFsTree = myFsTree;
+		}
+
+		@Override public void run() {
+			VirtualFile selectedFile = myFsTree.getSelectedFile();
+			if (selectedFile == null) return;
+
+			PsiFile psiFile = PsiManager.getInstance(project).findFile(selectedFile);
+			if (psiFile == null) return;
+
+			disableHighlightingFor(psiFile);
+		}
+
+		private static void disableHighlightingFor(PsiFile psiFile) {
+			FileViewProvider viewProvider = psiFile.getViewProvider();
+			List<Language> languages = new ArrayList<Language>(viewProvider.getLanguages());
+			Collections.sort(languages, PsiUtilBase.LANGUAGE_COMPARATOR);
+
+			for (Language language : languages) {
+				PsiElement root = viewProvider.getPsi(language);
+				HighlightLevelUtil.forceRootHighlighting(root, FileHighlighingSetting.SKIP_HIGHLIGHTING);
+			}
+			DaemonCodeAnalyzer analyzer = DaemonCodeAnalyzer.getInstance(psiFile.getProject());
+			analyzer.restart();
 		}
 	}
 }
