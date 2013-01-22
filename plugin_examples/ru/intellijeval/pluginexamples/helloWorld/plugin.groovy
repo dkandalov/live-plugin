5d82b440100ef68327ce72e07c3d2918d77a305a
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-01-22 00:09:02 +0000

    improving helloWorld example

--- plugin_examples/ru/intellijeval/pluginexamples/helloWorld/plugin.groovy
+++ plugin_examples/ru/intellijeval/pluginexamples/helloWorld/plugin.groovy
@@ -1,16 +1,19 @@
-import com.intellij.notification.*
-import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.actionSystem.AnActionEvent
+import static ru.intellijeval.PluginUtil.show
 
-import javax.swing.SwingUtilities
-
-
-static show(String htmlBody, String title = "", NotificationType notificationType = NotificationType.INFORMATION) {
-    SwingUtilities.invokeLater({
-        def notification = new Notification("", title, htmlBody, notificationType)
-        ApplicationManager.application.messageBus.syncPublisher(Notifications.TOPIC).notify(notification)
-    } as Runnable)
-}
-
+// This should show balloon message with "Hello IntelliJ" text.
+// (If there is no balloon, it might be disabled in "IDE Settings - Notifications".)
 show("Hello IntelliJ")
 
-// You can find more examples in "Add Plugin - Example" menu
+
+// There are couple implicit variables available in "plugin.groovy" files.
+// One of them is "event" which contains information about action executing this script.
+show(((AnActionEvent) event))
+// It can be used to get reference to the current project (e.g. if IntelliJ API needs to know it).
+show(event.project)
+
+// "pluginPath" - absolute path to this plugin folder.
+show(pluginPath)
+
+// "isIdeStartup" - true on IDE startup if "Run all plugins on IDE start" option is enabled.
+show(isIdeStartup)
