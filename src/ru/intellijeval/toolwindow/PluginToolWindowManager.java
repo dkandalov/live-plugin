716bb9b836b5368ca820302e483539c940d135e8
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-01-30 22:44:20 +0000

    only add intellijeval/idea jars to modules without these dependencies

--- src/ru/intellijeval/toolwindow/PluginToolWindowManager.java
+++ src/ru/intellijeval/toolwindow/PluginToolWindowManager.java
@@ -53,6 +53,7 @@ import com.intellij.openapi.roots.libraries.Library;
 import com.intellij.openapi.roots.libraries.LibraryTable;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.ui.SimpleToolWindowPanel;
+import com.intellij.openapi.util.Condition;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.Ref;
 import com.intellij.openapi.util.io.FileUtil;
@@ -81,7 +82,10 @@ import com.intellij.util.ui.tree.TreeUtil;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import ru.intellijeval.*;
+import ru.intellijeval.EvalComponent;
+import ru.intellijeval.EvaluatePluginAction;
+import ru.intellijeval.Settings;
+import ru.intellijeval.Util;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultTreeModel;
@@ -819,19 +823,22 @@ public class PluginToolWindowManager {
 
 	private static class DependenciesUtil {
 		public static boolean allModulesHasLibraryAsDependencyIn(Project project, String libraryName) {
-			for (Module module : ModuleManager.getInstance(project).getModules()) {
-				ModifiableRootModel moduleRootManager = ModuleRootManager.getInstance(module).getModifiableModel();
-				Library library = moduleRootManager.getModuleLibraryTable().getLibraryByName(libraryName);
-				if (library == null) return false;
-			}
-			return true;
+			return findModulesWithoutLibrary(ModuleManager.getInstance(project).getModules(), libraryName).isEmpty();
 		}
 
 		public static void removeLibraryDependencyFrom(final Project project, final String libraryName) {
 			ApplicationManager.getApplication().runWriteAction(new Runnable() {
 				@Override public void run() {
-					for (Module module : ModuleManager.getInstance(project).getModules()) {
-						removeDependencyFrom(module, libraryName);
+					Module[] modules = ModuleManager.getInstance(project).getModules();
+					for (Module module : findModulesWithLibrary(modules, libraryName)) {
+
+						ModifiableRootModel moduleRootManager = ModuleRootManager.getInstance(module).getModifiableModel();
+						LibraryTable libraryTable = moduleRootManager.getModuleLibraryTable();
+
+						Library library = libraryTable.getLibraryByName(libraryName);
+						if (library != null) libraryTable.removeLibrary(library);
+						moduleRootManager.commit();
+
 					}
 				}
 			});
@@ -840,38 +847,48 @@ public class PluginToolWindowManager {
 		public static void addLibraryDependencyTo(final Project project, final String libraryName, final String... paths) {
 			ApplicationManager.getApplication().runWriteAction(new Runnable() {
 				@Override public void run() {
-					for (Module module : ModuleManager.getInstance(project).getModules()) {
-						addDependencyTo(module, libraryName, paths);
+					Module[] modules = ModuleManager.getInstance(project).getModules();
+					for (Module module : findModulesWithoutLibrary(modules, libraryName)) {
+
+						ModifiableRootModel moduleRootManager = ModuleRootManager.getInstance(module).getModifiableModel();
+						LibraryTable libraryTable = moduleRootManager.getModuleLibraryTable();
+
+						Library library = libraryTable.createLibrary(libraryName);
+						Library.ModifiableModel modifiableLibrary = library.getModifiableModel();
+						for (String path : paths) {
+							modifiableLibrary.addRoot(path, OrderRootType.CLASSES);
+						}
+						modifiableLibrary.commit();
+
+						LibraryOrderEntry libraryOrderEntry = moduleRootManager.findLibraryOrderEntry(library);
+						if (libraryOrderEntry != null) libraryOrderEntry.setScope(DependencyScope.PROVIDED);
+						moduleRootManager.commit();
+
 					}
 				}
 			});
 		}
 
-		private static void removeDependencyFrom(Module module, String libraryName) {
-			ModifiableRootModel moduleRootManager = ModuleRootManager.getInstance(module).getModifiableModel();
-			LibraryTable libraryTable = moduleRootManager.getModuleLibraryTable();
-
-			Library library = libraryTable.getLibraryByName(libraryName);
-			if (library == null) return;
-			libraryTable.removeLibrary(library);
-
-			moduleRootManager.commit();
+		private static List<Module> findModulesWithoutLibrary(Module[] modules, final String libraryName) {
+			return ContainerUtil.findAll(modules, new Condition<Module>() {
+				@Override public boolean value(Module module) {
+					return !dependsOn(libraryName, module);
+				}
+			});
 		}
 
-		private static void addDependencyTo(Module module, String libraryName, String... paths) {
+		private static List<Module> findModulesWithLibrary(Module[] modules, final String libraryName) {
+			return ContainerUtil.findAll(modules, new Condition<Module>() {
+				@Override public boolean value(Module module) {
+					return dependsOn(libraryName, module);
+				}
+			});
+		}
+
+		private static boolean dependsOn(String libraryName, Module module) {
 			ModifiableRootModel moduleRootManager = ModuleRootManager.getInstance(module).getModifiableModel();
-			LibraryTable libraryTable = moduleRootManager.getModuleLibraryTable();
-
-			Library library = libraryTable.createLibrary(libraryName);
-			Library.ModifiableModel modifiableLibrary = library.getModifiableModel();
-			for (String path : paths) {
-				modifiableLibrary.addRoot(path, OrderRootType.CLASSES);
-			}
-			modifiableLibrary.commit();
-
-			LibraryOrderEntry libraryOrderEntry = moduleRootManager.findLibraryOrderEntry(library);
-			if (libraryOrderEntry != null) libraryOrderEntry.setScope(DependencyScope.PROVIDED);
-			moduleRootManager.commit();
+			Library library = moduleRootManager.getModuleLibraryTable().getLibraryByName(libraryName);
+			return library != null;
 		}
 	}
 
