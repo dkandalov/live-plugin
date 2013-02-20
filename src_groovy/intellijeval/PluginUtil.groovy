fd9a519248950db5d485760509840f8b38ce8a9d
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-02-20 09:44:58 +0000

    missing import

--- src_groovy/intellijeval/PluginUtil.groovy
+++ src_groovy/intellijeval/PluginUtil.groovy
@@ -59,6 +59,7 @@ import com.intellij.unscramble.UnscrambleDialog
 import com.intellij.util.ui.UIUtil
 import org.jetbrains.annotations.NotNull
 import org.jetbrains.annotations.Nullable
+import org.junit.Test
 
 import javax.swing.*
 import java.awt.*
