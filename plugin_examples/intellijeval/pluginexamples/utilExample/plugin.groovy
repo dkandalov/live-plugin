9ad6413cddc531b365ebaa9bbfa9f40498309fc5
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-06-24 18:01:55 +0100

    simplified showInConsole() to always open a new one (otherwise it's too error prone)

--- plugin_examples/intellijeval/pluginexamples/utilExample/plugin.groovy
+++ plugin_examples/intellijeval/pluginexamples/utilExample/plugin.groovy
@@ -15,9 +15,8 @@ show("IntelliJ", "Hello")
 show(AClass.sayHello(), "Hello")
 
 // shows text in console
-// (note that text is appended to exisiting console with the same title)
 showInConsole("Hello console", "my console", project)
 showInConsole("....", "my console", project)
 
-// shows exception's stacktrace in new console
-showInNewConsole(new Exception("This is a fake exception to show exception in a console"), "console with exception", event.project)
+// shows exception's stacktrace
+showInConsole(new Exception("This is a fake exception to show exception in a console"), "console with exception", event.project)
