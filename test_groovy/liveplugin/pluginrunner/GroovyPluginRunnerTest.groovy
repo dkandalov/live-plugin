ea2804fea8d59e2c1d565317c06f5e66fc503ed2
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-06-30 17:07:56 +0100

    more test for GroovyPluginRunner

--- test_groovy/liveplugin/pluginrunner/GroovyPluginRunnerTest.groovy
+++ test_groovy/liveplugin/pluginrunner/GroovyPluginRunnerTest.groovy
@@ -1,6 +1,8 @@
 package liveplugin.pluginrunner
 
 import com.intellij.openapi.util.io.FileUtil
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
 class GroovyPluginRunnerTest {
@@ -9,6 +11,7 @@ class GroovyPluginRunnerTest {
 
 	private ErrorReporter errorReporter = new ErrorReporter()
 	private GroovyPluginRunner pluginRunner = new GroovyPluginRunner(errorReporter, NO_ENVIRONMENT)
+	private File directory
 
 	@Test void "should run correct groovy script without errors"() {
 		def scriptCode = """
@@ -20,35 +23,54 @@ class GroovyPluginRunnerTest {
 			def b = 2
 			a + b
 """
-		givenScriptWith(scriptCode) { File file ->
-			pluginRunner.runGroovyScript(file.absolutePath, file.parentFile.absolutePath, "someId", NO_BINDING)
+		def file = createFile("plugin.groovy", scriptCode)
+		pluginRunner.runGroovyScript(file.absolutePath, file.parentFile.absolutePath, "someId", NO_BINDING)
 
-			assert collectErrorsFrom(errorReporter).empty
-		}
+		assert collectErrorsFrom(errorReporter).empty
 	}
 
 	@Test void "should run incorrect groovy script with errors"() {
 		def scriptCode = """
 			invalid code + 1
 """
-		givenScriptWith(scriptCode) { File file ->
-			pluginRunner.runGroovyScript(file.absolutePath, file.parentFile.absolutePath, "someId", NO_BINDING)
+		def file = createFile("plugin.groovy", scriptCode)
+		pluginRunner.runGroovyScript(file.absolutePath, file.parentFile.absolutePath, "someId", NO_BINDING)
 
-			def errors = collectErrorsFrom(errorReporter)
-			assert errors.size() == 1
-			assert errors.first()[0] == "someId"
-			assert errors.first()[1].startsWith("groovy.lang.MissingPropertyException")
-		}
+		def errors = collectErrorsFrom(errorReporter)
+		assert errors.size() == 1
+		assert errors.first()[0] == "someId"
+		assert errors.first()[1].startsWith("groovy.lang.MissingPropertyException")
 	}
 
-	private static givenScriptWith(String sourceCode, Closure callback) {
-		def file = FileUtil.createTempFile("", "")
-		file.write(sourceCode)
-		try {
-			callback(file)
-		} finally {
-			file.delete()
-		}
+	@Test void "should run groovy script which uses groovy class from another file"() {
+		def scriptCode = """
+			Util.myFunction()
+"""
+		def scriptCode2 = """
+			class Util {
+				static myFunction() { 42 }
+			}
+"""
+		def file = createFile("plugin.groovy", scriptCode)
+		createFile("Util.groovy", scriptCode2)
+
+		pluginRunner.runGroovyScript(file.absolutePath, directory.absolutePath, "someId", NO_BINDING)
+
+		assert collectErrorsFrom(errorReporter).empty
+	}
+
+	@Before void setup() {
+		directory = FileUtil.createTempDirectory("", "")
+	}
+
+	@After void teardown() {
+		directory.delete()
+	}
+
+	def createFile(String fileName, String fileContent) {
+		def file = new File(directory, fileName)
+		file.write(fileContent)
+		file
 	}
 
 	private def static collectErrorsFrom(ErrorReporter errorReporter) {
