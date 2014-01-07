d607d09a58fc273f3425214ac74d2cb95c11e4a4
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-01-07 18:15:51 +0400

    creating test classes which take project as parameter

--- src_groovy/liveplugin/IntegrationTestsRunner.groovy
+++ src_groovy/liveplugin/IntegrationTestsRunner.groovy
@@ -7,19 +7,21 @@ import org.junit.Test
 @SuppressWarnings(["GroovyUnusedDeclaration"])
 class IntegrationTestsRunner {
 	static def runIntegrationTests(Project project, List<Class> testClasses) {
-		def result = testClasses.collect{ runTestsInClass(it) }.join("\n\n")
+		def result = testClasses.collect{ runTestsInClass(it, project) }.join("\n\n")
 		PluginUtil.showInConsole(result, "Integration tests", project)
 	}
 
 	// TODO use standard intellij unit test window
 
-	private static String runTestsInClass(Class testClass) {
+	private static String runTestsInClass(Class testClass, Project project) {
 		def testMethods = testClass.declaredMethods.findAll{
 			it.annotations.find{ it instanceof Test } != null && it.annotations.find{ it instanceof Ignore } == null
 		}
 
 		testMethods.collect{ method ->
-			testClass.simpleName + ": " + runTest(method.name){ method.invoke(testClass.newInstance()) }
+			testClass.simpleName + ": " + runTest(method.name){
+				method.invoke(createInstanceOf(testClass, project))
+			}
 		}.join("\n")
 	}
 
@@ -34,5 +36,17 @@ class IntegrationTestsRunner {
 			assertionError.printStackTrace(new PrintWriter(writer))
 			methodName + " - FAILED \n" + writer.buffer.toString()
 		}
+	}
+
+	private static Object createInstanceOf(Class testClass, Project project) {
+		def hasDefaultConstructor = testClass.constructors.any{ it.parameterTypes.size() == 0 }
+		if (hasDefaultConstructor) return testClass.newInstance()
+
+		def hasConstructorWithProject = testClass.constructors.any{
+			it.parameterTypes.size() == 1 && it.parameterTypes.first() == Project
+		}
+		if (hasConstructorWithProject) return testClass.newInstance(project)
+
+		throw new IllegalStateException("Failed to create test class ${testClass}")
 	}
 }
