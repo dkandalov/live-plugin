5f7f7938dfc1691178b24d79e26b40534b301531
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-01-07 19:12:38 +0400

    pass context of plugin test as a map

--- src_groovy/liveplugin/IntegrationTestsRunner.groovy
+++ src_groovy/liveplugin/IntegrationTestsRunner.groovy
@@ -1,26 +1,29 @@
 package liveplugin
 
 import com.intellij.openapi.project.Project
+import org.jetbrains.annotations.NotNull
+import org.jetbrains.annotations.Nullable
 import org.junit.Ignore
 import org.junit.Test
 
 @SuppressWarnings(["GroovyUnusedDeclaration"])
 class IntegrationTestsRunner {
-	static def runIntegrationTests(Project project, List<Class> testClasses) {
-		def result = testClasses.collect{ runTestsInClass(it, project) }.join("\n\n")
+	static def runIntegrationTests(List<Class> testClasses, @NotNull Project project, @Nullable String pluginPath = null) {
+		def context = [project: project, pluginPath: pluginPath]
+		def result = testClasses.collect{ runTestsInClass(it, context) }.join("\n\n")
 		PluginUtil.showInConsole(result, "Integration tests", project)
 	}
 
 	// TODO use standard intellij unit test window
 
-	private static String runTestsInClass(Class testClass, Project project) {
+	private static String runTestsInClass(Class testClass, Map context) {
 		def testMethods = testClass.declaredMethods.findAll{
 			it.annotations.find{ it instanceof Test } != null && it.annotations.find{ it instanceof Ignore } == null
 		}
 
 		testMethods.collect{ method ->
 			testClass.simpleName + ": " + runTest(method.name){
-				method.invoke(createInstanceOf(testClass, project))
+				method.invoke(createInstanceOf(testClass, context))
 			}
 		}.join("\n")
 	}
@@ -38,14 +41,14 @@ class IntegrationTestsRunner {
 		}
 	}
 
-	private static Object createInstanceOf(Class testClass, Project project) {
+	private static Object createInstanceOf(Class testClass, Map context) {
+		def hasConstructorWithContext = testClass.constructors.any{
+			it.parameterTypes.size() == 1 && it.parameterTypes.first() == Map
+		}
+		if (hasConstructorWithContext) return testClass.newInstance(context)
+
 		def hasDefaultConstructor = testClass.constructors.any{ it.parameterTypes.size() == 0 }
 		if (hasDefaultConstructor) return testClass.newInstance()
-
-		def hasConstructorWithProject = testClass.constructors.any{
-			it.parameterTypes.size() == 1 && it.parameterTypes.first() == Project
-		}
-		if (hasConstructorWithProject) return testClass.newInstance(project)
 
 		throw new IllegalStateException("Failed to create test class ${testClass}")
 	}
