17a19548c4510e4bc700355c93c801f16ccdb9a2
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-11-19 00:47:00 +0000

    cleaned up inspection example

--- plugin_examples/liveplugin/pluginexamples/helloWorldInspection/plugin.groovy
+++ plugin_examples/liveplugin/pluginexamples/helloWorldInspection/plugin.groovy
@@ -1,22 +1,19 @@
 import com.intellij.codeInsight.daemon.GroupNames
-import com.intellij.codeInspection.*
-import com.intellij.codeInspection.ex.InspectionProfileImpl
-import com.intellij.codeInspection.ex.InspectionToolRegistrar
-import com.intellij.codeInspection.ex.InspectionToolWrapper
-import com.intellij.codeInspection.ex.LocalInspectionToolWrapper
-import com.intellij.ide.ui.search.SearchableOptionsRegistrar
+import com.intellij.codeInspection.BaseJavaLocalInspectionTool
+import com.intellij.codeInspection.LocalQuickFix
+import com.intellij.codeInspection.ProblemDescriptor
+import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.openapi.project.Project
-import com.intellij.profile.codeInspection.InspectionProjectProfileManager
 import com.intellij.psi.JavaElementVisitor
 import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.PsiElementVisitor
 import com.intellij.psi.PsiLiteralExpression
 
+import static liveplugin.PluginUtil.registerInspection
 import static liveplugin.PluginUtil.show
-
 // (Please note this example won't work in IDEs without Java support.)
 
-addInspection(project){ new HelloWorldInspection() }
+registerInspection(project, new HelloWorldInspection())
 show("Loaded hello world inspection<br/>It replaces \"hello\" string literal in Java code with \"Hello world\"")
 
 
@@ -47,28 +44,5 @@ class HelloWorldQuickFix implements LocalQuickFix {
 		def stringLiteral = factory.createExpressionFromText('"Hello World"', null)
 		descriptor.psiElement.replace(stringLiteral)
 	}
-}
-
-static addInspection(Project project, Closure inspectionFactory) {
-	def registrar = new InspectionToolRegistrar(SearchableOptionsRegistrar.instance)
-	registrar.registerInspectionToolFactory(new com.intellij.openapi.util.Factory<InspectionToolWrapper>() {
-		@Override InspectionToolWrapper create() {
-			new LocalInspectionToolWrapper((LocalInspectionTool) inspectionFactory.call())
-		}
-	}, true)
-
-	// there is also InspectionProfileManager which keeps global IDE profiles
-	def projectProfileManager = InspectionProjectProfileManager.getInstance(project)
-	def oldProfile = projectProfileManager.inspectionProfile
-
-	// create new profile to make it ask registrar for newly added inspection
-	def newProfile = new InspectionProfileImpl(oldProfile.name, registrar, projectProfileManager)
-	newProfile.copyFrom(oldProfile)
-	newProfile.baseProfile = null // can break if baseProfile is set to default profile? (e.g. in com.intellij.codeInspection.ex.InspectionProfileImpl#readExternal)
-	newProfile.initInspectionTools(null)
-
-	projectProfileManager.deleteProfile(oldProfile.name)
-	projectProfileManager.updateProfile(newProfile)
-	projectProfileManager.projectProfile = newProfile.name
 }
 
