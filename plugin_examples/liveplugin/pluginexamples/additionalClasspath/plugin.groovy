819b37e8eabea5cdb65f8964a3c5f8d4f20fd340
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2016-08-14 20:25:11 +0100

    misc

--- plugin_examples/liveplugin/pluginexamples/additionalClasspath/plugin.groovy
+++ plugin_examples/liveplugin/pluginexamples/additionalClasspath/plugin.groovy
@@ -3,14 +3,15 @@ import org.mockito.Mockito
 import static liveplugin.PluginUtil.show
 
 // add-to-classpath $HOME/.m2/repository/org/mockito/mockito-all/1.9.5/mockito*.jar
+
 if (!isIdeStartup) {
 	show(Mockito.class.name)
 }
 
-// To add additional jars to plugin classloader:
-// - There must be one or more lines which start with "// add-to-classpath " followed by path.
-// - Path should use "/" separator and can include glob wildcards.
-// - Any OS environment variable can be used with '$' as prefix (e.g. "$JAVA_HOME").
+// To add jars or folder with class files to plugin classloader:
+// - After "import" statements add one or more lines which start with "// add-to-classpath " followed by path.
+// - Path must have "/" separator and can include glob wildcards if you want to include multiple jars.
+// - OS environment variables can be used with '$' as prefix (e.g. "$JAVA_HOME")
 //   (additional $PLUGIN_PATH variable contains path to current plugin folder).
 // - Note that in plugin.groovy files there is intention to insert "add-to-classpath" comment
 //   (use alt+Enter to see popup with intentions)
