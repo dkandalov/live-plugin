edbde8f22ca5eb2685b6f692d83149ad8c968368
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2016-12-17 12:34:56 +0000

    Inspections api fix so that it works in IJ 2016.3

--- src_groovy/liveplugin/implementation/Inspections.groovy
+++ src_groovy/liveplugin/implementation/Inspections.groovy
@@ -4,13 +4,13 @@ import com.intellij.codeInspection.InspectionProfile
 import com.intellij.codeInspection.InspectionProfileEntry
 import com.intellij.codeInspection.ex.InspectionProfileImpl
 import com.intellij.codeInspection.ex.InspectionToolRegistrar
-import com.intellij.codeInspection.ex.InspectionToolWrapper
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
-import com.intellij.openapi.util.Factory
 import com.intellij.profile.codeInspection.InspectionProfileManager
 import com.intellij.profile.codeInspection.InspectionProjectProfileManager
+
+import java.util.function.Supplier
 
 import static liveplugin.implementation.Misc.accessField
 import static liveplugin.implementation.Misc.newDisposable
@@ -94,23 +94,19 @@ class Inspections {
 	}
 
 	private static def inspectionsProfileOf(Project project) {
-		InspectionProjectProfileManager.getInstance(project).inspectionProfile as InspectionProfileImpl
+		InspectionProjectProfileManager.getInstance(project).getCurrentProfile() as InspectionProfileImpl
 	}
 
 	private static void reloadAdditionalProjectInspections(Project project, List<InspectionProfileEntry> inspections) {
 		def inspectionFactories = inspections.collect { inspection ->
-			new Factory<InspectionToolWrapper>() {
-				@Override InspectionToolWrapper create() {
-					InspectionToolRegistrar.wrapTool(inspection)
-				}
-			}
+			{ -> InspectionToolRegistrar.wrapTool(inspection) } as Supplier
 		}
 		def registrar = new InspectionToolRegistrar()
 		def obfuscatedNames = ["a", "b", "c"] // "abc" because there three fields in the class
 		def toolFactories = accessField(registrar, ["myInspectionToolFactories"] + obfuscatedNames, List)
 		toolFactories.addAll(inspectionFactories)
 
-		def projectProfile = InspectionProjectProfileManager.getInstance(project).inspectionProfile
+		def projectProfile = InspectionProjectProfileManager.getInstance(project).getCurrentProfile()
 		def newProfile = updateProfile(projectProfile, registrar, project)
 		inspections.each{ newProfile.enableTool(it.shortName, project) }
 	}
@@ -129,10 +125,10 @@ class Inspections {
 	 *    Also it's not clear what is the difference between base and parent profiles in {@link com.intellij.codeInspection.ModifiableModel}.)
 	 */
 	private static InspectionProfileImpl updateProfile(InspectionProfile profile, InspectionToolRegistrar registrar, Project project) {
-		def appProfileManager = InspectionProfileManager.instance
+		def appProfileManager = InspectionProfileManager.getInstance()
 
-		if (profile.modifiableModel.baseProfileName == null || profile.modifiableModel.baseProfileName == profile.name) {
-			def rootProfile = appProfileManager.rootProfile
+		if (profile.getModifiableModel().getBaseProfile() == null || profile.getModifiableModel().getBaseProfile().name == profile.name) {
+			def rootProfile = appProfileManager.getCurrentProfile()
 
 			def newRootProfile = new InspectionProfileImpl(rootProfile.name, registrar, appProfileManager)
 			newRootProfile.copyFrom(rootProfile)
@@ -145,15 +141,14 @@ class Inspections {
 		} else {
 			def projectProfileManager = InspectionProjectProfileManager.getInstance(project)
 
-			def baseProfile = appProfileManager.getProfile(profile.modifiableModel.baseProfileName) as InspectionProfile
-			if (baseProfile == null) baseProfile = projectProfileManager.getProfile(profile.modifiableModel.baseProfileName) as InspectionProfile
+			def baseProfile = appProfileManager.getProfile(profile.modifiableModel.getBaseProfile().name) as InspectionProfile
+			if (baseProfile == null) baseProfile = projectProfileManager.getProfile(profile.modifiableModel.getBaseProfile().name) as InspectionProfile
 			if (baseProfile != profile) {
 				updateProfile(baseProfile, registrar, project)
 			}
 
-			def projectProfile = projectProfileManager.inspectionProfile
-			def projectBaseProfile = appProfileManager.getProfile(projectProfile.modifiableModel.baseProfileName) as InspectionProfile
-			def newProjectProfile = new InspectionProfileImpl(projectProfile.name, registrar, projectProfileManager, projectBaseProfile)
+			def projectProfile = projectProfileManager.getCurrentProfile()
+			def newProjectProfile = new InspectionProfileImpl(projectProfile.name, registrar, projectProfileManager)
 			newProjectProfile.copyFrom(projectProfile)
 			newProjectProfile.initInspectionTools(project)
 
@@ -162,6 +157,4 @@ class Inspections {
 			newProjectProfile
 		}
 	}
-
-
 }
