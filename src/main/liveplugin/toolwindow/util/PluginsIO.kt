722b861895eab57c865c83f64de3343b6ffefea6
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-10-25 09:13:33 +0100

    avoid exceptions when adding example files which already exist

--- src/main/liveplugin/toolwindow/util/PluginsIO.kt
+++ src/main/liveplugin/toolwindow/util/PluginsIO.kt
@@ -12,31 +12,33 @@ object PluginsIO {
     @Throws(IOException::class)
     @JvmStatic fun createFile(parentPath: String, fileName: String, text: String) {
         runIOAction("createFile", {
-            val parentFolder = VfsUtil.createDirectoryIfMissing(parentPath) ?: throw IOException("Failed to create folder " + parentPath)
-            val file = parentFolder.createChildData(requestor, fileName)
-            VfsUtil.saveText(file, text)
+            val parentFolder = VfsUtil.createDirectoryIfMissing(parentPath) ?: throw IOException("Failed to create folder $parentPath")
+            if (parentFolder.findChild(fileName) == null) {
+                val file = parentFolder.createChildData(requestor, fileName)
+                VfsUtil.saveText(file, text)
+            }
         })
     }
 
     @Throws(IOException::class)
     @JvmStatic fun delete(filePath: String) {
         runIOAction("delete", {
-            val file = VirtualFileManager.getInstance().findFileByUrl("file://" + filePath) ?: throw IOException("Failed to find file " + filePath)
+            val file = VirtualFileManager.getInstance().findFileByUrl("file://" + filePath) ?: throw IOException("Failed to find file $filePath")
             file.delete(requestor)
         })
     }
 
     private fun runIOAction(actionName: String, f: () -> Unit) {
         var exception: IOException? = null
-        CommandProcessor.getInstance().executeCommand(null, {
-            ApplicationManager.getApplication().runWriteAction {
+        ApplicationManager.getApplication().runWriteAction {
+            CommandProcessor.getInstance().executeCommand(null, {
                 try {
                     f()
                 } catch (e: IOException) {
                     exception = e
                 }
-            }
-        }, actionName, "LivePlugin")
+            }, actionName, "LivePlugin")
+        }
 
         if (exception != null) throw exception!!
     }
