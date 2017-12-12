e0a44f2189cc36862c5fdc689b976f99ebebb037
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-12-12 13:44:28 +0000

    resize toolwindow icon

--- src/main/liveplugin/Icons.kt
+++ src/main/liveplugin/Icons.kt
@@ -2,6 +2,7 @@ package liveplugin
 
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.util.IconLoader
+import com.intellij.util.IconUtil.toSize
 import com.intellij.util.ui.UIUtil
 import javax.swing.Icon
 
@@ -11,9 +12,11 @@ object Icons {
     val newPluginIcon = pluginIcon
     val deletePluginIcon = AllIcons.General.Remove!!
     val refreshPluginsPanelIcon = AllIcons.Actions.Refresh!!
-    val pluginToolwindowIcon: Icon =
+    val pluginToolwindowIcon: Icon = toSize(
         if (UIUtil.isUnderDarcula()) AllIcons.Nodes.Plugin // Custom darker icon so that it looks ok as toolwindow icon with default (white) look-and-feel.
-        else IconLoader.getIcon("/liveplugin/plugin-toolwindow-icon.png")
+        else IconLoader.getIcon("/liveplugin/plugin-toolwindow-icon.png"),
+        13, 13 // resize because IJ logs warning if toolwindow icon is not 13x13
+    )
     val runPluginIcon = AllIcons.Actions.Execute!!
     val testPluginIcon = AllIcons.RunConfigurations.Junit!!
     val expandAllIcon = AllIcons.Actions.Expandall!!
