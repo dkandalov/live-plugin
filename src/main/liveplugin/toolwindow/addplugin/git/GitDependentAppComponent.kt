22f9b59527a4e867b3f85a8e4c7559d273ac8d69
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-04-02 12:44:45 +0100

    don't use deprecated ApplicationComponent

--- src/main/liveplugin/toolwindow/addplugin/git/GitDependentAppComponent.kt
+++ src/main/liveplugin/toolwindow/addplugin/git/GitDependentAppComponent.kt
@@ -1,18 +1,11 @@
 package liveplugin.toolwindow.addplugin.git
 
-import com.intellij.openapi.components.ApplicationComponent
-import com.intellij.openapi.project.DumbAware
 import liveplugin.toolwindow.addplugin.addFromGistAction
 import liveplugin.toolwindow.addplugin.addFromGitHubAction
 
-class GitDependentAppComponent: ApplicationComponent, DumbAware {
-
-    override fun initComponent() {
+class GitDependentAppComponent {
+    init {
         addFromGistAction = AddPluginFromGistAction()
         addFromGitHubAction = AddPluginFromGitAction()
     }
-
-    override fun disposeComponent() {}
-
-    override fun getComponentName() = "GitDependentAppComponent"
 }
