c2b9da15ad067e3b7562bd2b83010439bc5824b6
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-06-05 20:18:57 +0100

    updated toolwindow icon

--- src/main/liveplugin/Icons.kt
+++ src/main/liveplugin/Icons.kt
@@ -1,27 +1,28 @@
 package liveplugin
 
-import com.intellij.icons.AllIcons
+import com.intellij.icons.AllIcons.*
 import com.intellij.openapi.util.IconLoader
 import com.intellij.util.IconUtil.toSize
-import com.intellij.util.ui.UIUtil
-import javax.swing.Icon
+import javax.swing.UIManager
 
 object Icons {
-    val pluginIcon = AllIcons.Nodes.Plugin!!
-    val addPluginIcon = AllIcons.General.Add!!
+    val pluginIcon = Nodes.Plugin!!
+    val addPluginIcon = General.Add!!
     val newPluginIcon = pluginIcon
-    val deletePluginIcon = AllIcons.General.Remove!!
-    val refreshPluginsPanelIcon = AllIcons.Actions.Refresh!!
-    val pluginToolwindowIcon: Icon = toSize(
-        if (UIUtil.isUnderDarcula()) AllIcons.Nodes.Plugin // Custom darker icon so that it looks ok as toolwindow icon with default (white) look-and-feel.
-        else IconLoader.getIcon("/liveplugin/plugin-toolwindow-icon.png"),
+    val deletePluginIcon = General.Remove!!
+    val refreshPluginsPanelIcon = Actions.Refresh!!
+    val pluginToolwindowIcon = toSize(
+        IconLoader.getIcon(if (isUnderDarcula()) "/liveplugin/plugin_dark.svg" else "/liveplugin/plugin.svg"),
         13, 13 // resize because IJ logs warning if toolwindow icon is not 13x13
     )
-    val runPluginIcon = AllIcons.Actions.Execute!!
-    val testPluginIcon = AllIcons.RunConfigurations.Junit!!
-    val expandAllIcon = AllIcons.Actions.Expandall!!
-    val collapseAllIcon = AllIcons.Actions.Collapseall!!
-    val settingsIcon = AllIcons.General.GearPlain!!
-    val helpIcon = AllIcons.Actions.Help!!
-    val newFolderIcon = AllIcons.Nodes.Folder!!
+    val runPluginIcon = Actions.Execute!!
+    val testPluginIcon = RunConfigurations.Junit!!
+    val expandAllIcon = Actions.Expandall!!
+    val collapseAllIcon = Actions.Collapseall!!
+    val settingsIcon = General.GearPlain!!
+    val helpIcon = Actions.Help!!
+    val newFolderIcon = Nodes.Folder!!
 }
+
+// Copied from UIUtil to keep plugin working while the function is being moved to StartupUiUtil
+private fun isUnderDarcula() = UIManager.getLookAndFeel().name.contains("Darcula")
