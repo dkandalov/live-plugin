64868092b8312d20d75cb251829e2f9c94e89682
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-06-10 21:42:41 +0100

    cleanup

--- src/main/liveplugin/toolwindow/addplugin/git/AddPluginFromGistAction.kt
+++ src/main/liveplugin/toolwindow/addplugin/git/AddPluginFromGistAction.kt
@@ -15,7 +15,7 @@ import liveplugin.IdeUtil.showErrorDialog
 import liveplugin.LivePluginAppComponent.Companion.livePluginsPath
 import liveplugin.toolwindow.RefreshPluginsPanelAction
 import liveplugin.toolwindow.addplugin.PluginIdValidator
-import liveplugin.toolwindow.addplugin.git.AddPluginFromGistAction.Companion.GistUrlValidator.Companion.extractGistIdFrom
+import liveplugin.toolwindow.addplugin.git.AddPluginFromGistAction.GistUrlValidator.Companion.extractGistIdFrom
 import liveplugin.toolwindow.util.createFile
 import org.jetbrains.plugins.github.api.GithubApiRequest
 import org.jetbrains.plugins.github.api.GithubApiRequestExecutor
@@ -29,12 +29,12 @@ import javax.swing.Icon
 class AddPluginFromGistAction: AnAction("Copy from Gist", "Copy from Gist", AllIcons.Vcs.Vendors.Github), DumbAware {
 
     override fun actionPerformed(event: AnActionEvent) {
-        val project = event.project
-        val gistUrl = askUserForGistUrl(event) ?: return
+        val project = event.project ?: return
+        val gistUrl = askUserForGistUrl(project) ?: return
 
         fetchGistFrom(
             gistUrl,
-            event,
+            project,
             onSuccess = { gist ->
                 val newPluginId = askUserNewPluginName(project)
                 if (newPluginId != null) {
@@ -52,95 +52,94 @@ class AddPluginFromGistAction: AnAction("Copy from Gist", "Copy from Gist", AllI
         )
     }
 
+    private fun askUserForGistUrl(project: Project): String? =
+        Messages.showInputDialog(
+            project,
+            "Enter gist URL:",
+            dialogTitle,
+            defaultIcon,
+            "",
+            GistUrlValidator()
+        )
+
+    private fun askUserNewPluginName(project: Project?): String? =
+        Messages.showInputDialog(project, "Enter new plugin name:", dialogTitle, defaultIcon, "", PluginIdValidator())
+
+    private fun createPluginFrom(gist: GithubGist, pluginId: String?) =
+        gist.files.forEach { gistFile ->
+            createFile("$livePluginsPath/$pluginId", gistFile.filename, gistFile.content)
+        }
+
     private fun showMessageThatCreatingPluginFailed(e: IOException, newPluginId: String?, project: Project?) {
         showErrorDialog(project, "Error adding plugin \"$newPluginId\" to $livePluginsPath", dialogTitle)
         log.info(e)
+    }
+
+    private fun showMessageThatFetchingGistFailed(e: IOException?, project: Project?) {
+        showErrorDialog(project, "Failed to fetch gist", dialogTitle)
+        log.info(e!!)
+    }
+
+    private fun fetchGistFrom(
+        gistUrl: String,
+        project: Project,
+        onSuccess: (GithubGist) -> Unit,
+        onFailure: (IOException) -> Unit
+    ) {
+        object: Task.Backgroundable(project, "Fetching Gist", false, ALWAYS_BACKGROUND) {
+            private var gist: GithubGist? = null
+            private var exception: IOException? = null
+
+            override fun run(indicator: ProgressIndicator) {
+                try {
+                    val gistId = extractGistIdFrom(gistUrl)
+                    val request = GithubApiRequests.Gists.get(
+                        server = GithubServerPath.DEFAULT_SERVER,
+                        id = gistId
+                    )
+                    gist = SimpleExecutor().execute(request)
+
+                } catch (e: IOException) {
+                    exception = e
+                }
+            }
+
+            override fun onSuccess() {
+                val e = exception
+                if (e != null) onFailure(e) else onSuccess(gist!!)
+            }
+        }.queue()
+    }
+
+    private class SimpleExecutor: GithubApiRequestExecutor.Base(GithubSettings.getInstance()) {
+        override fun <T> execute(indicator: ProgressIndicator, request: GithubApiRequest<T>): T =
+            createRequestBuilder(request).execute(request, indicator)
+    }
+
+    private class GistUrlValidator: InputValidatorEx {
+        private var errorText: String? = null
+
+        override fun checkInput(inputString: String): Boolean {
+            val isValid = inputString.lastIndexOf('/') != -1
+            errorText = if (isValid) null else "Gist URL should have at least one '/' symbol"
+            return isValid
+        }
+
+        override fun getErrorText(inputString: String) = errorText
+
+        override fun canClose(inputString: String) = true
+
+        companion object {
+            fun extractGistIdFrom(gistUrl: String): String {
+                val i = gistUrl.lastIndexOf('/')
+                return gistUrl.substring(i + 1)
+            }
+        }
     }
 
     private companion object {
         private val log = Logger.getInstance(AddPluginFromGistAction::class.java)
         private const val dialogTitle = "Copy Plugin From Gist"
         private val defaultIcon: Icon? = null
-
-        private fun askUserForGistUrl(event: AnActionEvent): String? =
-            Messages.showInputDialog(
-                event.project,
-                "Enter gist URL:",
-                dialogTitle,
-                defaultIcon,
-                "",
-                GistUrlValidator()
-            )
-
-        private fun fetchGistFrom(
-            gistUrl: String,
-            event: AnActionEvent,
-            onSuccess: (GithubGist) -> Unit,
-            onFailure: (IOException) -> Unit
-        ) {
-            val project = event.project ?: return
-            object: Task.Backgroundable(project, "Fetching Gist", false, ALWAYS_BACKGROUND) {
-                private var gist: GithubGist? = null
-                private var exception: IOException? = null
-
-                override fun run(indicator: ProgressIndicator) {
-                    try {
-                        val gistId = extractGistIdFrom(gistUrl)
-                        val request = GithubApiRequests.Gists.get(
-                            server = GithubServerPath.DEFAULT_SERVER,
-                            id = gistId
-                        )
-                        gist = SimpleExecutor().execute(request)
-
-                    } catch (e: IOException) {
-                        exception = e
-                    }
-                }
-
-                override fun onSuccess() {
-                    val e = exception
-                    if (e != null) onFailure(e) else onSuccess(gist!!)
-                }
-            }.queue()
-        }
-
-        private fun askUserNewPluginName(project: Project?): String? =
-            Messages.showInputDialog(project, "Enter new plugin name:", dialogTitle, defaultIcon, "", PluginIdValidator())
-
-        private fun createPluginFrom(gist: GithubGist, pluginId: String?) =
-            gist.files.forEach { gistFile ->
-                createFile("$livePluginsPath/$pluginId", gistFile.filename, gistFile.content)
-            }
-
-        private fun showMessageThatFetchingGistFailed(e: IOException?, project: Project?) {
-            showErrorDialog(project, "Failed to fetch gist", dialogTitle)
-            log.info(e!!)
-        }
-
-        private class SimpleExecutor: GithubApiRequestExecutor.Base(GithubSettings.getInstance()) {
-            override fun <T> execute(indicator: ProgressIndicator, request: GithubApiRequest<T>): T =
-                createRequestBuilder(request).execute(request, indicator)
-        }
-
-        private class GistUrlValidator: InputValidatorEx {
-            private var errorText: String? = null
-
-            override fun checkInput(inputString: String): Boolean {
-                val isValid = inputString.lastIndexOf('/') != -1
-                errorText = if (isValid) null else "Gist URL should have at least one '/' symbol"
-                return isValid
-            }
-
-            override fun getErrorText(inputString: String) = errorText
-
-            override fun canClose(inputString: String) = true
-
-            companion object {
-                fun extractGistIdFrom(gistUrl: String): String {
-                    val i = gistUrl.lastIndexOf('/')
-                    return gistUrl.substring(i + 1)
-                }
-            }
-        }
     }
 }
