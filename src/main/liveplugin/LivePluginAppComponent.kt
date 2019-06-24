15b3ce26145705c4caa514e692fe2edaa91b855a
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-06-25 00:16:52 +0100

    Added UseScopeExtension (issue #94), UsageTypeExtension and Highlighter

--- src/main/liveplugin/LivePluginAppComponent.kt
+++ src/main/liveplugin/LivePluginAppComponent.kt
@@ -1,7 +1,6 @@
 package liveplugin
 
-import com.intellij.codeInsight.daemon.impl.analysis.DefaultHighlightingSettingProvider
-import com.intellij.codeInsight.daemon.impl.analysis.FileHighlightingSetting
+import com.intellij.lang.LanguageUtil
 import com.intellij.notification.NotificationGroup
 import com.intellij.notification.NotificationListener
 import com.intellij.notification.NotificationType
@@ -12,12 +11,26 @@ import com.intellij.openapi.application.PathManager.getHomePath
 import com.intellij.openapi.application.PathManager.getPluginsPath
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.fileEditor.impl.NonProjectFileWritingAccessExtension
+import com.intellij.openapi.fileTypes.FileType
+import com.intellij.openapi.fileTypes.SyntaxHighlighter
+import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory
+import com.intellij.openapi.fileTypes.SyntaxHighlighterProvider
+import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.Project.DIRECTORY_STORE_FOLDER
+import com.intellij.openapi.util.NotNullLazyKey
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.util.io.FileUtilRt.toSystemIndependentName
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiElement
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.LocalSearchScope
+import com.intellij.psi.search.SearchScope
+import com.intellij.psi.search.UseScopeEnlarger
+import com.intellij.psi.util.PsiUtilCore
+import com.intellij.usages.impl.rules.UsageType
+import com.intellij.usages.impl.rules.UsageTypeProvider
 import com.intellij.util.indexing.IndexableSetContributor
 import liveplugin.IdeUtil.askIfUserWantsToRestartIde
 import liveplugin.IdeUtil.downloadFile
@@ -177,19 +190,51 @@ class LivePluginAppComponent: DumbAware {
             ExamplePluginInstaller(groovyExamplesPath + "popup-menu/", listOf("plugin.groovy")).installPlugin(loggingListener)
         }
     }
+
+    class Highlighter: SyntaxHighlighterProvider {
+        override fun create(fileType: FileType, project: Project?, file: VirtualFile?): SyntaxHighlighter? {
+            if (project == null || file == null || !FileUtil.startsWith(file.path, livePluginsPath)) return null
+            val language = LanguageUtil.getLanguageForPsi(project, file)
+            return if (language == null) null else SyntaxHighlighterFactory.getSyntaxHighlighter(language, project, file)
+        }
+    }
+
+    class UsageTypeExtension: UsageTypeProvider {
+        override fun getUsageType(element: PsiElement): UsageType? {
+            val file = PsiUtilCore.getVirtualFile(element) ?: return null
+            if (!FileUtil.startsWith(file.path, livePluginsPath)) return null
+            return UsageType("Usage in liveplugin")
+        }
+    }
+
+    class UseScopeExtension: UseScopeEnlarger() {
+        override fun getAdditionalUseScope(element: PsiElement): SearchScope? {
+            val useScope = element.useScope
+            return if (useScope is LocalSearchScope) null else LivePluginsSearchScope.getScopeInstance(element.project)
+        }
+    }
+
+    private class LivePluginsSearchScope(project: Project): GlobalSearchScope(project) {
+        override fun getDisplayName() = "LivePlugins"
+        override fun contains(file: VirtualFile) = FileUtil.startsWith(file.path, livePluginsPath)
+        override fun isSearchOutsideRootModel() = true
+        override fun isSearchInModuleContent(aModule: Module) = false
+        override fun isSearchInLibraries() = false
+
+        companion object {
+            private val SCOPE_KEY = NotNullLazyKey.create<LivePluginsSearchScope, Project>("LIVEPLUGIN_SEARCH_SCOPE_KEY") { project ->
+                LivePluginsSearchScope(project)
+            }
+
+            fun getScopeInstance(project: Project): GlobalSearchScope = SCOPE_KEY.getValue(project)
+        }
+    }
 }
 
 class LivePluginIndexableSetContributor: IndexableSetContributor() {
     override fun getAdditionalRootsToIndex(): MutableSet<VirtualFile> {
         val path = livePluginsPath.findFileByUrl() ?: return HashSet()
         return mutableSetOf(path)
-    }
-}
-
-class EnableHighlightingForLivePlugins: DefaultHighlightingSettingProvider(), DumbAware {
-    override fun getDefaultSetting(project: Project, file: VirtualFile): FileHighlightingSetting? {
-        val isUnderPluginsRootPath = FileUtil.startsWith(file.path, livePluginsPath)
-        return if (isUnderPluginsRootPath) FileHighlightingSetting.FORCE_HIGHLIGHTING else null
     }
 }
 
