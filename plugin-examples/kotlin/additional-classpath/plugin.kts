75d4c9b83e1894534c58fd4a5ed33f6e0fb7b2fb
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-08-06 14:49:11 +0100

    updated additional-classpath example for Kotlin

--- plugin-examples/kotlin/additional-classpath/plugin.kts
+++ plugin-examples/kotlin/additional-classpath/plugin.kts
@@ -1,7 +1,27 @@
 import liveplugin.*
 
 // add-to-classpath $HOME/.m2/repository/org/mockito/mockito-core/3.3.3/mockito*.jar
+// depends-on-plugin Subversion
 
 if (!isIdeStartup) {
-    show(org.mockito.Mockito.Mockito::class.java.name)
+    show(org.mockito.Mockito::class.java.name)
+    show(org.jetbrains.idea.svn.SvnVcs::class.java.name)
 }
+
+// You can add jars or folder with class files to plugin classloader:
+// - After "import" statements insert "// add-to-classpath " followed by a path.
+// - Path must have "/" separators and can include glob wildcards if you want to include multiple jars.
+// - Use '$' as prefix (e.g. "$JAVA_HOME") to refer to OS environment variables
+//   (additional $PLUGIN_PATH variable contains path to the current plugin folder).
+
+// You can use classes from other IDE plugins by specifying dependency on that plugin:
+// - After "import" statements insert "// depends-on-plugin " followed by plugin id
+//   which can be found in the plugin's "META-INF/plugin.xml".
+
+// Note that:
+// - "plugin.kts" files have intentions to insert "// add-to-classpath" and "// depends-on-plugin"
+//   (use alt+Enter to see popup with intentions)
+// - New classloader is created each time plugin is executed.
+//   It means that addition classpath library classes are loaded on each plugin execution and
+//   won't be unloaded if there are objects referencing them or classloader.
+//   See also https://docs.oracle.com/javase/specs/jls/se7/html/jls-12.html#jls-12.7
