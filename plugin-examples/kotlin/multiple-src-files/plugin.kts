2278e8b00023edfcceffe9c88300589be573629f
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2021-03-16 22:47:22 +0000

    updated example

--- plugin-examples/kotlin/multiple-src-files/plugin.kts
+++ plugin-examples/kotlin/multiple-src-files/plugin.kts
@@ -3,3 +3,6 @@ import liveplugin.show
 
 show(foo())
 show(bar())
+
+// Unfortunately, at the moment Kotlin syntax highlighting only works within one file.
+// For details and updated please see https://github.com/dkandalov/live-plugin/issues/105.
