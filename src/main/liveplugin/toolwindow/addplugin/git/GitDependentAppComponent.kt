a31c7a8ca0a3e47b2851f5b55657fe1a8becb53f
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2021-04-06 22:01:46 +0100

    don't use ApplicationComponents

--- src/main/liveplugin/toolwindow/addplugin/git/GitDependentAppComponent.kt
+++ src/main/liveplugin/toolwindow/addplugin/git/GitDependentAppComponent.kt
@@ -1,10 +1,11 @@
 package liveplugin.toolwindow.addplugin.git
 
+import com.intellij.ide.AppLifecycleListener
 import liveplugin.toolwindow.addplugin.addFromGistAction
 import liveplugin.toolwindow.addplugin.addFromGitHubAction
 
-class GitDependentAppComponent {
-    init {
+class GitDependentAppComponent : AppLifecycleListener {
+    override fun appFrameCreated(commandLineArgs: MutableList<String>) {
         addFromGistAction = AddPluginFromGistAction()
         addFromGitHubAction = AddPluginFromGitAction()
     }
--- src/plugin-util-groovy/liveplugin/testrunner/IntegrationTestsRunner.groovy
+++ src/plugin-util-groovy/liveplugin/testrunner/IntegrationTestsRunner.groovy
@@ -1,6 +1,6 @@
 package liveplugin.testrunner
 
-import com.intellij.openapi.components.ApplicationComponent
+import com.intellij.ide.AppLifecycleListener
 import com.intellij.openapi.project.Project
 import liveplugin.PluginUtil
 import org.jetbrains.annotations.NotNull
@@ -10,10 +10,15 @@ import org.junit.Test
 
 import java.lang.reflect.Method
 
-
-class IntegrationTestsRunner implements ApplicationComponent {
+class IntegrationTestsRunner implements AppLifecycleListener {
     // by default use text runner because it will work in all IntelliJ IDEs
 	private static runTests = IntegrationTestsTextRunner.&runIntegrationTests
+
+	@Override void appFrameCreated(@NotNull List<String> commandLineArgs) {
+		// only initialized if there is junit plugin (which might not be the case for some IntelliJ IDEs)
+		runTests = IntegrationTestsUIRunner.&runIntegrationTests
+		PluginUtil.log("Enabled junit panel output for integration tests")
+	}
 
 	/**
 	 * Runs tests in specified {@code testClasses} reporting results using
@@ -30,17 +35,6 @@ class IntegrationTestsRunner implements ApplicationComponent {
 	static void runIntegrationTests(List<Class> testClasses, @NotNull Project project, @Nullable String pluginPath = null) {
 		runTests(testClasses, project, pluginPath)
 	}
-
-	@Override void initComponent() {
-        // only initialized if there is junit plugin (which might not be the case for some IntelliJ IDEs)
-		runTests = IntegrationTestsUIRunner.&runIntegrationTests
-		PluginUtil.log("Enabled junit panel output for integration tests")
-	}
-
-	@Override void disposeComponent() {}
-
-	@Override String getComponentName() { this.class.simpleName }
-
 
 	static runTestsInClass(Class testClass, Map context, TestReporter testReport, Closure<Long> now) {
 		def isTest = { Method method -> method.annotations.find{ it instanceof Test} }
@@ -96,9 +90,8 @@ class IntegrationTestsRunner implements ApplicationComponent {
 		if (hasConstructorWithContext) return testClass.newInstance(context)
 
 		def hasDefaultConstructor = testClass.constructors.any{ it.parameterTypes.size() == 0 }
-		if (hasDefaultConstructor) return testClass.newInstance()
+		if (hasDefaultConstructor) return testClass.getDeclaredConstructor().newInstance()
 
 		throw new IllegalStateException("Failed to create test class ${testClass}")
 	}
-
 }
