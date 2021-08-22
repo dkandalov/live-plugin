0c0df60d1e7557461f5df2cc288252d4b3576b89
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2021-08-22 13:13:45 +0100

    don't run project specific plugin if the project is not trusted

--- src/main/liveplugin/LivePluginProjectListener.kt
+++ src/main/liveplugin/LivePluginProjectListener.kt
@@ -1,11 +1,14 @@
 package liveplugin
 
+import com.intellij.ide.impl.isTrusted
+import com.intellij.notification.NotificationType.INFORMATION
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.Presentation
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
+import liveplugin.LivePluginAppComponent.Companion.livePluginNotificationGroup
 import liveplugin.pluginrunner.RunPluginAction
 import liveplugin.pluginrunner.UnloadPluginAction
 
@@ -16,6 +19,13 @@ class LivePluginProjectListener : ProjectManagerListener {
         val projectPath = project.basePath?.toFilePath() ?: return
         val pluginsPath = projectPath + LivePluginPaths.livePluginsProjectDirName
         if (!pluginsPath.exists()) return
+
+        @Suppress("UnstableApiUsage")
+        if (!project.isTrusted()) {
+            val message = "Skipped execution of project specific plugins because the project is not trusted."
+            livePluginNotificationGroup.createNotification(title = "Live Plugin", message, INFORMATION).notify(project)
+            return
+        }
 
         val dataContext = MapDataContext(mapOf(CommonDataKeys.PROJECT.name to project))
         val dummyEvent = AnActionEvent(null, dataContext, "", Presentation(), ActionManager.getInstance(), 0)
