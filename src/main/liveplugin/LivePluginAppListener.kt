c009e45a1df7485cd3871ea5c0d2e94830c4554b
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2021-09-12 11:34:14 +0100

    non-modal groovy download, correct groovy lib url, refactoring

--- src/main/liveplugin/LivePluginAppListener.kt
+++ src/main/liveplugin/LivePluginAppListener.kt
@@ -2,17 +2,25 @@ package liveplugin
 
 import com.intellij.ide.AppLifecycleListener
 import com.intellij.notification.NotificationType
+import com.intellij.notification.NotificationType.ERROR
 import com.intellij.openapi.application.ex.ApplicationManagerEx
 import com.intellij.openapi.ui.Messages
-import com.intellij.util.containers.ContainerUtil
+import com.intellij.openapi.ui.Messages.OK
 import com.intellij.util.download.DownloadableFileService
+import liveplugin.GroovyDownloader.downloadGroovyJar
+import liveplugin.GroovyDownloader.isGroovyOnClasspath
+import liveplugin.IdeUtil.invokeLaterOnEDT
+import liveplugin.LivePluginAppComponent.Companion.livePluginNotificationGroup
+import liveplugin.LivePluginPaths.livePluginLibPath
 import liveplugin.toolwindow.util.GroovyExamples
 import liveplugin.toolwindow.util.installPlugin
+import java.util.concurrent.CompletableFuture
 
 class LivePluginAppListener: AppLifecycleListener {
     @Suppress("UnstableApiUsage")
     override fun appStarted() {
-        checkThatGroovyIsOnClasspath()
+        // Download Groovy in non-java IDEs because they don't have bundled Groovy libs.
+        if (!isGroovyOnClasspath()) downloadGroovyJar()
 
         val settings = Settings.instance
         if (settings.justInstalled) {
@@ -25,7 +33,7 @@ class LivePluginAppListener: AppLifecycleListener {
     }
 
     private fun installHelloWorldPlugins() {
-        IdeUtil.invokeLaterOnEDT {
+        invokeLaterOnEDT {
             listOf(GroovyExamples.helloWorld, GroovyExamples.ideActions, GroovyExamples.modifyDocument, GroovyExamples.popupMenu).forEach {
                 it.installPlugin(handleError = { e: Exception, pluginPath: String ->
                     LivePluginAppComponent.logger.warn("Failed to install plugin: $pluginPath", e)
@@ -34,52 +42,51 @@ class LivePluginAppListener: AppLifecycleListener {
         }
     }
 
-    private fun checkThatGroovyIsOnClasspath(): Boolean {
-        val isGroovyOnClasspath = isOnClasspath("org.codehaus.groovy.runtime.DefaultGroovyMethods")
-        if (isGroovyOnClasspath) return true
+}
 
-        // This can be useful for non-java IDEs because they don't have bundled groovy libs.
-        LivePluginAppComponent.livePluginNotificationGroup.createNotification(
+private object GroovyDownloader {
+    fun isGroovyOnClasspath() =
+        LivePluginAppListener::class.java.classLoader.getResource(
+            "org.codehaus.groovy.runtime.DefaultGroovyMethods".replace(".", "/") + ".class"
+        ) != null
+
+    fun downloadGroovyJar() {
+        livePluginNotificationGroup.createNotification(
             title = "LivePlugin didn't find Groovy libraries on classpath",
-            content = "Without it plugins won't work. <a href=\"\">Download Groovy libraries</a> (~7Mb)",
-            type = NotificationType.ERROR
+            content = "Without it plugins won't work. <a href=\"\">Download Groovy libraries</a> (~5Mb)",
+            type = ERROR
         ).setListener { notification, _ ->
-            val groovyVersion = "2.5.11" // Version of groovy used by latest IJ.
-            val downloaded = downloadFile(
-                "https://repo1.maven.org/maven2/org/codehaus/groovy/groovy-all/$groovyVersion/",
-                "groovy-all-$groovyVersion.jar",
-                LivePluginPaths.livePluginLibPath
-            )
-            if (downloaded) {
-                notification.expire()
-                askIfUserWantsToRestartIde("For Groovy libraries to be loaded IDE restart is required. Restart now?")
-            } else {
-                LivePluginAppComponent.livePluginNotificationGroup
-                    .createNotification("Failed to download Groovy libraries", NotificationType.WARNING)
+            val groovyVersion = "2.5.14" // Version of Groovy jar in the latest IJ.
+            downloadFiles(
+                url = "https://repo1.maven.org/maven2/org/codehaus/groovy/groovy/$groovyVersion/",
+                fileName = "groovy-$groovyVersion.jar",
+                targetPath = livePluginLibPath
+            ).whenComplete { downloaded, _ ->
+                if (downloaded) {
+                    notification.expire()
+                    val answer = Messages.showOkCancelDialog(
+                        "LivePlugin needs to restart IDE to load Groovy libraries. Restart now?",
+                        "IDE Restart",
+                        "Restart",
+                        "Postpone",
+                        Messages.getQuestionIcon()
+                    )
+                    if (answer == OK) {
+                        ApplicationManagerEx.getApplicationEx().restart(true)
+                    }
+                } else {
+                    livePluginNotificationGroup
+                        .createNotification("Failed to download Groovy libraries", NotificationType.WARNING)
+                }
             }
         }.notify(null)
-
-        return false
     }
 
-    private fun downloadFile(downloadUrl: String, fileName: String, targetPath: FilePath): Boolean =
-        downloadFiles(listOf(Pair(downloadUrl, fileName)), targetPath)
-
-    // TODO make download non-modal
-    private fun downloadFiles(urlAndFileNames: List<Pair<String, String>>, targetPath: FilePath): Boolean {
+    private fun downloadFiles(url: String, fileName: String, targetPath: FilePath): CompletableFuture<Boolean> {
         val service = DownloadableFileService.getInstance()
-        val descriptions = ContainerUtil.map(urlAndFileNames) { service.createFileDescription(it.first + it.second, it.second) }
-        val files = service.createDownloader(descriptions, "").downloadFilesWithProgress(targetPath.value, null, null)
-        return files != null && files.size == urlAndFileNames.size
+        val description = service.createFileDescription(url + fileName, fileName)
+        return service.createDownloader(listOf(description), "")
+            .downloadWithBackgroundProgress(targetPath.value, null)
+            .thenApply { it?.size == 1 }
     }
-
-    private fun askIfUserWantsToRestartIde(message: String) {
-        val answer = Messages.showOkCancelDialog(message, "Restart Is Required", "Restart", "Postpone", Messages.getQuestionIcon())
-        if (answer == Messages.OK) {
-            ApplicationManagerEx.getApplicationEx().restart(true)
-        }
-    }
-
-    private fun isOnClasspath(className: String) =
-        LivePluginAppListener::class.java.classLoader.getResource(className.replace(".", "/") + ".class") != null
 }
