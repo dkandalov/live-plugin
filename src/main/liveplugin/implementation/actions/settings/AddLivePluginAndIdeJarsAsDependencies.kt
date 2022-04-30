26a840c7036bbf3a577c27775caf57689bfe0b31
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2022-04-30 07:57:19 +0100

    moved dependencies-util.kt closer to its only usage

--- src/main/liveplugin/implementation/actions/settings/AddLivePluginAndIdeJarsAsDependencies.kt
+++ src/main/liveplugin/implementation/actions/settings/AddLivePluginAndIdeJarsAsDependencies.kt
@@ -2,18 +2,21 @@ package liveplugin.implementation.actions.settings
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.DependencyScope
 import com.intellij.openapi.roots.ModuleRootManager
+import com.intellij.openapi.roots.OrderRootType
 import com.intellij.openapi.roots.OrderRootType.CLASSES
 import com.intellij.openapi.roots.OrderRootType.SOURCES
+import com.intellij.openapi.roots.libraries.LibraryTable
 import com.intellij.util.PathUtil
 import liveplugin.implementation.LivePlugin
 import liveplugin.implementation.LivePluginPaths.ideJarsPath
 import liveplugin.implementation.LivePluginPaths.livePluginLibPath
-import liveplugin.implementation.toolwindow.util.addLibraryDependencyTo
-import liveplugin.implementation.toolwindow.util.removeLibraryDependencyFrom
 
 class AddLivePluginAndIdeJarsAsDependencies: AnAction(), DumbAware {
     private val livePluginAndIdeJarsLibrary = "LivePlugin and IDE jars (to enable navigation and auto-complete)"
@@ -74,3 +77,60 @@ class AddLivePluginAndIdeJarsAsDependencies: AnAction(), DumbAware {
         }
     }
 }
+
+private fun removeLibraryDependencyFrom(project: Project, libraryName: String) {
+    runWriteAction {
+        ModuleManager.getInstance(project).modules.forEach { module ->
+            if (module.dependsOn(libraryName)) {
+                module.removeDependencyOn(libraryName)
+            }
+        }
+    }
+}
+
+private fun addLibraryDependencyTo(project: Project, libraryName: String, paths: List<Pair<String, OrderRootType>>) {
+    runWriteAction {
+        val modules = ModuleManager.getInstance(project).modules
+        if (modules.isNotEmpty()) {
+            // Add dependency to the first module because this is enough for IntelliJ to see classes
+            // and adding dependency to all modules can be very slow for large projects
+            // (~16 seconds with UI freeze for IntelliJ source code).
+            modules[0].addDependencyOn(libraryName, paths)
+        }
+    }
+}
+
+private fun Module.removeDependencyOn(libraryName: String) {
+    val modifiableModel = ModuleRootManager.getInstance(this).modifiableModel
+    val libraryTable = modifiableModel.moduleLibraryTable
+
+    val library = libraryTable.getLibraryByName(libraryName)
+    if (library != null) libraryTable.removeLibrary(library)
+
+    modifiableModel.commit()
+}
+
+private fun Module.addDependencyOn(libraryName: String, paths: List<Pair<String, OrderRootType>>) {
+    val modifiableModel = ModuleRootManager.getInstance(this).modifiableModel
+    val libraryTable = modifiableModel.moduleLibraryTable
+    if (libraryTable.contains(libraryName)) return
+
+    val library = libraryTable.createLibrary(libraryName)
+    library.modifiableModel.let {
+        paths.forEach { (path, rootType) -> it.addRoot(path, rootType) }
+        it.commit()
+    }
+
+    val libraryOrderEntry = modifiableModel.findLibraryOrderEntry(library)
+    if (libraryOrderEntry != null) libraryOrderEntry.scope = DependencyScope.PROVIDED
+    modifiableModel.commit()
+}
+
+private fun Module.dependsOn(libraryName: String): Boolean {
+    val moduleRootManager = ModuleRootManager.getInstance(this).modifiableModel
+    val library = moduleRootManager.moduleLibraryTable.getLibraryByName(libraryName)
+    moduleRootManager.dispose()
+    return library != null
+}
+
+private fun LibraryTable.contains(libraryName: String) = getLibraryByName(libraryName) != null
