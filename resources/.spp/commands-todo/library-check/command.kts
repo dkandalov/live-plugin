ecf203e43d5c09f28cf86d01a93fd993e2236164
Brandon Fergerson <bfergerson@apache.org>
2022-05-16 15:33:28 +0400

    fix desc

--- resources/.spp/commands-todo/library-check/command.kts
+++ resources/.spp/commands-todo/library-check/command.kts
@@ -8,7 +8,7 @@ import spp.plugin.*
 class LibraryCheckCommand : LiveCommand() {
     override val name = "library-check"
     override val description = "<html><span style=\"color: ${getCommandTypeColor()}\">" +
-            "Find all jar libraries used the currently active services" + "</span></html>"
+            "Find all jar libraries used in the currently active services" + "</span></html>"
     override val selectedIcon: String = "library-check/icons/library-check_selected.svg"
     override val unselectedIcon: String = "library-check/icons/library-check_unselected.svg"
 
