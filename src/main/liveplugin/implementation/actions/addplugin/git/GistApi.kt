f4c06e4e9cdd7e535bca6436517684b5eb4d8de9
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2022-06-06 18:11:25 +0100

    added a basic GistApi using http4k (because built-in IJ gist API doesn't support updating and downloading gist versions)

---
+++ src/main/liveplugin/implementation/actions/addplugin/git/GistApi.kt
@@ -0,0 +1,128 @@
+package liveplugin.implementation.actions.addplugin.git
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility
+import com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.MapperFeature
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SNAKE_CASE
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.introspect.VisibilityChecker
+import com.fasterxml.jackson.module.kotlin.jacksonMapperBuilder
+import org.http4k.client.OkHttp
+import org.http4k.core.*
+import org.http4k.core.Method.*
+import org.http4k.core.Status.Companion.CREATED
+import org.http4k.core.Status.Companion.NO_CONTENT
+import org.http4k.core.Status.Companion.OK
+import org.http4k.filter.ClientFilters.SetBaseUriFrom
+import java.text.SimpleDateFormat
+import java.util.*
+
+interface GistApi {
+    // https://docs.github.com/en/rest/gists/gists#create-a-gist
+    fun create(gist: Gist, authToken: String): Gist
+
+    // https://docs.github.com/en/rest/gists/gists#update-a-gist
+    fun update(gist: Gist, authToken: String): Gist
+
+    // https://docs.github.com/en/rest/gists/gists#delete-a-gist
+    fun delete(gistId: String, authToken: String)
+
+    // https://docs.github.com/en/rest/gists/gists#get-a-gist
+    fun getGist(gistId: String): Gist
+
+    // https://docs.github.com/en/rest/gists/gists#list-gist-commits
+    fun listCommits(gistId: String): List<GistCommit>
+
+    // https://docs.github.com/en/rest/gists/gists#get-a-gist-revision
+    fun getGistRevision(gistId: String, sha: String): Gist
+
+    data class Gist(
+        val id: String = "",
+        val description: String = "",
+        val files: Map<String, GistFile>,
+        val public: Boolean = true
+    )
+
+    data class GistFile(val content: String)
+    data class GistCommit(val version: String)
+
+    companion object {
+        val gistClient = WithAcceptHeader().then(SetBaseUriFrom(Uri.of("https://api.github.com/gists")).then(OkHttp()))
+
+        operator fun invoke(httpHandler: HttpHandler = gistClient) = object : GistApi {
+            private val client = WithAcceptHeader().then(httpHandler)
+
+            override fun create(gist: Gist, authToken: String): Gist {
+                val request = Request(POST, "")
+                    .header("Authorization", "Bearer $authToken")
+                    .body(objectMapper.writeValueAsString(gist))
+                return client(request).expectStatus(CREATED).parse()
+            }
+
+            override fun update(gist: Gist, authToken: String): Gist {
+                val request = Request(PATCH, gist.id)
+                    .header("Authorization", "Bearer $authToken")
+                    .body(objectMapper.writeValueAsString(gist))
+                return client(request).expectStatus(OK).parse()
+            }
+
+            override fun delete(gistId: String, authToken: String) {
+                val request = Request(DELETE, gistId)
+                    .header("Authorization", "Bearer $authToken")
+                client(request).expectStatus(NO_CONTENT)
+            }
+
+            override fun getGist(gistId: String): Gist =
+                client(Request(GET, gistId)).expectStatus(OK).parse()
+
+            override fun listCommits(gistId: String): List<GistCommit> =
+                client(Request(GET, "$gistId/commits")).expectStatus(OK).parseList()
+
+            override fun getGistRevision(gistId: String, sha: String): Gist =
+                client(Request(GET, "$gistId/$sha")).expectStatus(OK).parse()
+
+            private fun Response.expectStatus(status: Status): Response {
+                if (this.status != status) error(body)
+                return this
+            }
+
+            private inline fun <reified T> Response.parseList(): List<T> =
+                objectMapper.readValue(
+                    body.stream,
+                    objectMapper.typeFactory.constructCollectionType(List::class.java, T::class.java)
+                )
+
+            private inline fun <reified T> Response.parse(): T =
+                objectMapper.readValue(
+                    body.stream,
+                    objectMapper.typeFactory.constructType(T::class.java)
+                )
+        }
+
+        private class WithAcceptHeader : Filter {
+            override fun invoke(handler: HttpHandler) = { request: Request ->
+                handler(request.header("Accept", "application/vnd.github.v3+json"))
+            }
+        }
+
+        private val objectMapper = jacksonMapperBuilder()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+            .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
+            .build()
+            .setDateFormat(SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX"))
+            .setTimeZone(TimeZone.getDefault())
+            .setSerializationInclusion(NON_NULL)
+            .setPropertyNamingStrategy(SNAKE_CASE)
+            .setVisibility(
+                VisibilityChecker.Std(
+                    /* getter = */ Visibility.NONE,
+                    /* isGetter = */ Visibility.NONE,
+                    /* setter = */ Visibility.NONE,
+                    /* creator = */ Visibility.NONE,
+                    /* field = */ Visibility.ANY
+                )
+            )
+    }
+}
---
+++ src/test/liveplugin/implementation/actions/git/GistApiTests.kt
@@ -0,0 +1,143 @@
+package liveplugin.implementation.actions.git
+
+import liveplugin.implementation.actions.addplugin.git.GistApi
+import liveplugin.implementation.actions.addplugin.git.GistApi.*
+import liveplugin.implementation.actions.addplugin.git.GistApi.Companion.gistClient
+import org.hamcrest.core.IsEqual.equalTo
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.then
+import org.http4k.filter.TrafficFilters.RecordTo
+import org.http4k.filter.TrafficFilters.ServeCachedFrom
+import org.http4k.traffic.ReadWriteCache
+import org.junit.Assert.assertThat
+import org.junit.Ignore
+import org.junit.Test
+import org.junit.jupiter.api.assertThrows
+
+@Ignore
+class GistApiExternalServerTests : GistApiTests(
+    GistApi(RecordTo(storage).then(gistClient)),
+    authToken = System.getenv("GIST_API_TOKEN") ?: ""
+)
+
+class GistApiReplayingServerTests : GistApiTests(
+    GistApi(ServeCachedFrom(storage).then { request: Request -> error("no recorded response for request:\n$request") }),
+    authToken = "dummy-token"
+)
+
+private val storage = ReadWriteCache.Disk("src/test/liveplugin/implementation/actions/git/recorded_traffic").sanitized()
+
+abstract class GistApiTests(
+    private val gistApi: GistApi,
+    private val authToken: String
+) {
+    @Test fun `create and delete gist`() {
+        val gist = Gist(description = "test", files = mapOf("test.txt" to GistFile("some file content")), public = false)
+        val createdGist = gistApi.create(gist, authToken)
+        assertThat(gist.copy(id = createdGist.id), equalTo(createdGist))
+
+        gistApi.delete(createdGist.id, authToken)
+    }
+
+    @Test fun `update gist and list its revisions`() {
+        val gist = gistApi.create(
+            Gist(description = "test", files = mapOf("test.txt" to GistFile("some file content")), public = false),
+            authToken
+        )
+        try {
+            val updatedGist = gistApi.update(gist.copy(files = mapOf("test.txt" to GistFile("updated file content"))), authToken)
+
+            val gistCommits = gistApi.listCommits(gist.id)
+            assertThat(gistCommits.size, equalTo(2))
+            val gists = gistCommits.map {
+                gistApi.getGistRevision(gist.id, sha = it.version)
+            }
+            assertThat(gists[0], equalTo(updatedGist))
+            assertThat(gists[1], equalTo(gist))
+        } finally {
+            gistApi.delete(gist.id, authToken)
+        }
+    }
+
+    @Test fun `get existing gist without authentication`() {
+        assertThat(
+            gistApi.getGist(gistId = "d51de2311dfd92dfa56feb3e3f9f96a6"),
+            equalTo(
+                Gist(
+                    id = "d51de2311dfd92dfa56feb3e3f9f96a6",
+                    description = "Workaround for https://youtrack.jetbrains.com/issue/IDEA-171273 (use https://github.com/dkandalov/live-plugin to run the code snippet)",
+                    files = mapOf(
+                        "plugin.kts" to GistFile(
+                            """
+                                import com.intellij.openapi.actionSystem.*
+            
+                                val newElementActionGroup = ActionManager.getInstance().getAction(IdeActions.GROUP_NEW) as DefaultActionGroup
+                                val newKotlinFileAction = ActionManager.getInstance().getAction("Kotlin.NewFile") as AnAction
+            
+                                newElementActionGroup.remove(newKotlinFileAction)
+                                newElementActionGroup.add(newKotlinFileAction, Constraints.FIRST)
+                                
+                            """.trimIndent()
+                        )
+                    )
+                )
+            )
+        )
+    }
+
+    @Test fun `list commits of existing gist without authentication`() {
+        assertThat(
+            gistApi.listCommits(gistId = "d51de2311dfd92dfa56feb3e3f9f96a6"),
+            equalTo(
+                listOf(
+                    GistCommit(version = "a6e4e0c2fe4664ce76c2cb5b5d22c60b3b484e70"),
+                    GistCommit(version = "d4375bcd40b8a241ba656c8162de1cfe478f7db0")
+                )
+            )
+        )
+    }
+
+    @Test fun `fail to create invalid gist`() {
+        assertThrows<IllegalStateException> {
+            gistApi.create(Gist(description = "test", files = emptyMap(), public = false), authToken)
+        }
+    }
+
+    @Test fun `fail to update non-existing gist`() {
+        assertThrows<IllegalStateException> {
+            gistApi.update(Gist(id = "invalid-gist-id", files = emptyMap()), authToken)
+        }
+    }
+
+    @Test fun `fail to delete non-existing gist`() {
+        assertThrows<IllegalStateException> {
+            gistApi.delete("invalid-gist-id", authToken)
+        }
+    }
+
+    @Test fun `fail to access non-existing gist`() {
+        assertThrows<IllegalStateException> { gistApi.getGist("invalid-gist-id") }
+        assertThrows<IllegalStateException> { gistApi.listCommits("invalid-gist-id") }
+        assertThrows<IllegalStateException> { gistApi.getGistRevision("invalid-gist-id", "abc") }
+    }
+}
+
+private fun ReadWriteCache.sanitized() = object : ReadWriteCache {
+    override fun set(request: Request, response: Response) {
+        this@sanitized[request.sanitized()] = response.sanitised()
+    }
+
+    override fun get(request: Request) = this@sanitized[request.sanitized()]
+
+    private fun Response.sanitised() = removeHeader("date")
+        .removeHeader("etag")
+        .removeHeader("last-modified")
+        .removeHeaders("x-")
+
+    private fun Request.sanitized() = replaceHeaderIfExists("Authorization", "Bearer dummy-token")
+
+    private fun Request.replaceHeaderIfExists(name: String, value: String?) =
+        if (header(name) == null) this
+        else removeHeader(name).header(name, value)
+}
