9d5d06ac8519dc8c509c2c76ced32235a740b294
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2022-06-07 23:04:39 +0100

    use GistApi in AddPluginFromGistAction

--- src/main/liveplugin/implementation/actions/addplugin/git/AddPluginFromGistAction.kt
+++ src/main/liveplugin/implementation/actions/addplugin/git/AddPluginFromGistAction.kt
@@ -1,6 +1,5 @@
 package liveplugin.implementation.actions.addplugin.git
 
-
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
@@ -13,20 +12,15 @@ import com.intellij.openapi.ui.InputValidatorEx
 import com.intellij.openapi.ui.Messages
 import liveplugin.implementation.LivePluginPaths.livePluginsPath
 import liveplugin.implementation.actions.addplugin.PluginIdValidator
-import liveplugin.implementation.actions.addplugin.git.AddPluginFromGistAction.GistUrlValidator.Companion.extractGistIdFrom
+import liveplugin.implementation.actions.addplugin.git.GistApi.FailedRequest
+import liveplugin.implementation.actions.addplugin.git.GistApi.Gist
 import liveplugin.implementation.common.IdeUtil.runLaterOnEdt
 import liveplugin.implementation.common.IdeUtil.showErrorDialog
 import liveplugin.implementation.common.createFile
-import org.jetbrains.plugins.github.api.GithubApiRequest
-import org.jetbrains.plugins.github.api.GithubApiRequestExecutor
-import org.jetbrains.plugins.github.api.GithubApiRequests
-import org.jetbrains.plugins.github.api.GithubServerPath
-import org.jetbrains.plugins.github.api.data.GithubGist
-import org.jetbrains.plugins.github.util.GithubSettings
 import java.io.IOException
 import javax.swing.Icon
 
-class AddPluginFromGistAction: AnAction("Copy from Gist", "Copy from Gist", AllIcons.Vcs.Vendors.Github), DumbAware {
+class AddPluginFromGistAction : AnAction("Copy from Gist", "Copy from Gist", AllIcons.Vcs.Vendors.Github), DumbAware {
 
     override fun actionPerformed(event: AnActionEvent) {
         val project = event.project ?: return
@@ -61,7 +55,7 @@ class AddPluginFromGistAction: AnAction("Copy from Gist", "Copy from Gist", AllI
             GistUrlValidator()
         )
 
-    private fun askUserForNewPluginName(project: Project, gist: GithubGist): String? =
+    private fun askUserForNewPluginName(project: Project, gist: Gist): String? =
         Messages.showInputDialog(
             project,
             "Enter new plugin name:",
@@ -71,12 +65,12 @@ class AddPluginFromGistAction: AnAction("Copy from Gist", "Copy from Gist", AllI
             PluginIdValidator()
         )
 
-    private fun createPluginFrom(gist: GithubGist, pluginId: String) =
-        gist.files.forEach { gistFile ->
-            createFile("$livePluginsPath/$pluginId", gistFile.filename, gistFile.content)
+    private fun createPluginFrom(gist: Gist, pluginId: String) =
+        gist.files.forEach { (filename, file) ->
+            createFile("$livePluginsPath/$pluginId", filename, file.content)
         }
 
-    private fun showMessageThatFetchingGistFailed(e: IOException?, project: Project) {
+    private fun showMessageThatFetchingGistFailed(e: FailedRequest?, project: Project) {
         showErrorDialog(project, "Failed to fetch gist", dialogTitle)
         if (e != null) log.info(e)
     }
@@ -89,31 +83,22 @@ class AddPluginFromGistAction: AnAction("Copy from Gist", "Copy from Gist", AllI
     private fun fetchGist(
         gistUrl: String,
         project: Project,
-        onSuccess: (GithubGist) -> Unit,
-        onFailure: (IOException?) -> Unit
+        onSuccess: (Gist) -> Unit,
+        onFailure: (FailedRequest?) -> Unit
     ) {
-        object: Task.Backgroundable(project, @Suppress("DialogTitleCapitalization") "Fetching Gist", true, ALWAYS_BACKGROUND) {
+        object : Task.Backgroundable(project, "Fetching gist…", true, ALWAYS_BACKGROUND) {
             override fun run(indicator: ProgressIndicator) {
                 try {
-                    val gistId = extractGistIdFrom(gistUrl)!!
-                    val request = GithubApiRequests.Gists.get(GithubServerPath.DEFAULT_SERVER, gistId)
-                    val gist = SimpleExecutor().execute(request)
-                    runLaterOnEdt {
-                        if (gist == null) onFailure(null) else onSuccess(gist)
-                    }
-                } catch (e: IOException) {
+                    val gist = GistApi().getGist(extractGistIdFrom(gistUrl)!!)
+                    runLaterOnEdt { onSuccess(gist) }
+                } catch (e: FailedRequest) {
                     runLaterOnEdt { onFailure(e) }
                 }
             }
         }.queue()
     }
 
-    private class SimpleExecutor: GithubApiRequestExecutor.Base(GithubSettings.getInstance()) {
-        override fun <T> execute(indicator: ProgressIndicator, request: GithubApiRequest<T>): T =
-            createRequestBuilder(request).execute(request, indicator)
-    }
-
-    private class GistUrlValidator: InputValidatorEx {
+    private class GistUrlValidator : InputValidatorEx {
         private var isValid = true
 
         override fun checkInput(inputString: String): Boolean {
@@ -125,19 +110,16 @@ class AddPluginFromGistAction: AnAction("Copy from Gist", "Copy from Gist", AllI
             if (isValid) null else "Couldn't parse gist URL"
 
         override fun canClose(inputString: String) = true
-
-        companion object {
-            fun extractGistIdFrom(gistUrl: String): String? {
-                val i = gistUrl.lastIndexOf('/')
-                return if (i == -1) null
-                else gistUrl.substring(i + 1)
-            }
-        }
     }
 
     private companion object {
         private val log = Logger.getInstance(AddPluginFromGistAction::class.java)
         private const val dialogTitle = "Copy Plugin From Gist"
         private val defaultIcon: Icon? = null
+
+        private fun extractGistIdFrom(gistUrl: String): String? {
+            val i = gistUrl.lastIndexOf('/')
+            return if (i == -1) null else gistUrl.substring(i + 1)
+        }
     }
 }
