eb27b62cd1ce137fef88774eb9dd8f97c4d1a071
Alexandr Popov <a.popov@squaregps.com>
2024-05-28 16:04:05 +0400

    added reading plugins from all project modules

--- src/main/liveplugin/implementation/LivePluginProjectListener.kt
+++ src/main/liveplugin/implementation/LivePluginProjectListener.kt
@@ -2,10 +2,11 @@ package liveplugin.implementation
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
+import kotlinx.coroutines.runBlocking
 import liveplugin.implementation.pluginrunner.PluginRunner.Companion.unloadPlugins
 
 class LivePluginProjectListener : ProjectManagerListener {
-    override fun projectClosing(project: Project) {
+    override fun projectClosing(project: Project) = runBlocking {
         unloadPlugins(readLivePlugins(project))
     }
 }
